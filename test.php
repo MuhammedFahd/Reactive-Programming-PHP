3acfd4efca3e7b05f0315b18f4ab51e5b31d9897
fahd fazal <fahd@xgengroup.com.au>
2021-07-27 15:15:14 +0530

    configured RxPHP

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Rx\Observable;
+use React\EventLoop\Factory;
+use Rx\Scheduler;
+
+$loop = Factory::create();
+
+//You only need to set the default scheduler once
+Scheduler::setDefaultFactory(function() use($loop){
+    return new Scheduler\EventLoopScheduler($loop);
+});
+
+Observable::interval(1000)
+    ->take(5)
+    ->flatMap(function ($i) {
+        return Observable::of($i + 1);
+    })
+    ->subscribe(function ($e) {
+        echo $e, PHP_EOL;
+    });
+
+$loop->run();
+
+?>
